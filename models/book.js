19bd51601a510654f6d1346e5d169216830f7c7f
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-07-25 17:03:45 -0400

    complete and exceeds

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@
 
 module.exports = (sequelize, DataTypes) => {
   const Book = sequelize.define('Book', {
-    title: {type: DataTypes.STRING, validate: {notEmpty: {msg: "Title is required"}}},
+    title: {type: DataTypes.STRING, validate: {notEmpty: {msg: "Title is required "}}},
     author: {type: DataTypes.STRING, validate: {notEmpty: {msg: "Author is required"}}},
     genre: DataTypes.STRING,
     year: DataTypes.INTEGER
